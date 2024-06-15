b0eec48784ba97fd757c62527b41727395888ada
Riyansh Jain <103922191+Riyanshj@users.noreply.github.com>
2024-06-15 15:34:26 +0530

    Delete text-to-speech directory

--- text-to-speech/content.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'extractText') {
-      sendResponse({ text: document.body.innerText });
-    }
-  });
-  
